2410a5e5fe1a46760b3887504e234b941c7e302f
Андрей <andrews.ru83@gmail.com>
2022-07-20 22:15:28 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
+    Author author1 = new Author("Lev", "Tolstoy");
+    Author author2 = new Author("Andrey", "Tarkovsky");
+    Author author3 = new Author("Andrey", "Tarkovsky");
+    Book book1 = new Book("War and Peace", author1, 2001);
+    Book book2 = new Book("Stalker", author2, 1999);
+    Book book3 = new Book("Stalker", author2, 1999);
 
-        System.out.println("Hello world!");
+        System.out.println(book1);
+        System.out.println(book2);
+
+        book1.setYearOfPublication(1980);
+
+        System.out.println(book1);
+
+        System.out.println(book2.getNameBook() + book2.getAuthor() + book2.getYearOfPublication());
+
+        System.out.println(book2.equals(book3));
+        System.out.println(author3.equals(author1));
+
+
     }
 }
